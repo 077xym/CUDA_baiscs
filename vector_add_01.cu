5c9e1ca39ef5158e01311db47b0a9197b48ec375
077xym <125954022+077xym@users.noreply.github.com>
2025-05-22 18:59:04 -0700

    Update vector_add_01.cu

--- vector_add_01.cu
+++ vector_add_01.cu
@@ -28,12 +28,28 @@ __global__ void vector_add_gpu(float *a, float *b, float *c, int n) {
 
         Since whatever the grid and dim size we chose, they will implicitly be transformed to
         3D, we can always use what is shown in 01_index.cu to get offset. However, we reduce
-        our operation counts because we know some of the dim will be statically 1.
+        our operation counts because we know some of the dim will be statically 0.
     */
     int thread_offset = blockIdx.x * blockDim.x + threadIdx.x;
     if (thread_offset < n) {
         c[thread_offset] = a[thread_offset] + b[thread_offset];
     }   
+}
+
+// CUDA kernel for GPU vector addition 3D
+__global__ void vector_add_gpu_3d(float *a, float *b, float *c, int n) {
+    int block_offset = 
+        blockIdx.x * gridDim.y * gridDim.z +
+        blockIdx.y * gridDim.z +
+        blockIdx.z;
+
+    int thread_offset = 
+        block_offset * blockDim.x +
+        threadIdx.x;
+    
+    if (thread_offset < n) {
+        c[thread_offset] = a[thread_offset] + b[thread_offset];
+    }
 }
 
 // Init vector with random values
@@ -54,9 +70,9 @@ double get_time() {
 int main() {
     
     // claim the host data variables, remember, these are the data stored in cpu
-    float *h_a, *h_b, *h_c_cpu, *h_c_gpu;
+    float *h_a, *h_b, *h_c_cpu, *h_c_gpu, *h_c_gpu_3d;
     // claim the device data variables, these are the data stored in gpu, copied from cpu
-    float *d_a, *d_b, *d_c;
+    float *d_a, *d_b, *d_c, *d_c_3d;
     // size_t, how many mem are going to be allocated
     size_t size = N * sizeof(float);
 
@@ -65,6 +81,7 @@ int main() {
     h_b = (float *)malloc(size);
     h_c_cpu = (float *)malloc(size);
     h_c_gpu = (float *)malloc(size);
+    h_c_gpu_3d = (float *)malloc(size);
 
     // randomly initialize the vector
     srand(time(NULL)); // set the seed
@@ -75,6 +92,7 @@ int main() {
     cudaMalloc(&d_a, size);
     cudaMalloc(&d_b, size);
     cudaMalloc(&d_c, size);
+    cudaMalloc(&d_c_3d, size);
 
     // copy data from cpu to gpu
     cudaMemcpy(d_a, h_a, size, cudaMemcpyHostToDevice);
@@ -104,6 +122,24 @@ int main() {
     }
     printf("GPU average time: %f seconds\n", gpu_time / 20.0);
 
+    /*
+        Let's define grid to be 2D (cause (N + BLOCK_SIZE - 1) / BLOCK_SIZE can only be divisible by 3)
+        but we can have a 1 at any axes to make it a 3D
+    */
+    dim3 grid_dim(3, 13021, 1);
+    dim3 block_dim(BLOCK_SIZE, 1, 1);
+    printf("Benchmarking GPU-3D implementation...\n");
+    double gpu_time_3d = 0.0;
+    for (int i = 0; i < 20; i++) {
+        double stime = get_time();
+        vector_add_gpu_3d<<<grid_dim, block_dim>>>(d_a, d_b, d_c_3d, N);
+        cudaDeviceSynchronize();
+        double etime = get_time();
+        gpu_time_3d += etime - stime;
+    }
+    printf("GPU average time: %f seconds\n", gpu_time_3d / 20.0);
+
+
     // execute cpu-version vector addition, and report the time
     printf("Benchmarking CPU implementation...\n");
     double cpu_time = 0.0;
@@ -125,16 +161,31 @@ int main() {
             break;
         }
     }
-    printf("Results are %s\n", correct ? "correct" : "incorrect");
+    printf("Results 1d are %s\n", correct ? "correct" : "incorrect");
+
+    // check results are equal
+    cudaMemcpy(h_c_gpu_3d, d_c_3d, size, cudaMemcpyDeviceToHost);
+    bool correct_3d = true;
+    for (int i = 0; i < N; i++) {
+        if (fabs(h_c_cpu[i] - h_c_gpu_3d[i]) > 1e-2) {
+            correct_3d = false;
+            printf("index: %d incorrect. cpu: %f, gpu: %f\n", i, h_c_cpu[i], h_c_gpu_3d[i]);
+            break;
+        }
+    }
+    printf("Results 3d are %s\n", correct_3d ? "correct" : "incorrect");
+    printf("%f", h_c_gpu_3d[769]);
 
     // free memory
     free(h_a);
     free(h_b);
     free(h_c_cpu);
     free(h_c_gpu);
+    free(h_c_gpu_3d);
     cudaFree(d_a);
     cudaFree(d_b);
     cudaFree(d_c);
+    cudaFree(d_c_3d);
 
     return 0;
 }
